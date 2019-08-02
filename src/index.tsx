d7ed9129198485497826305052005adcd4ffd67d
Alex Stelea <xstelea@gmail.com>
2019-08-02 21:43:28 +0200

    build: update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,18 @@
-import ReactDOM from "react-dom";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
 
-import { createMuiTheme } from "@material-ui/core";
-import * as React from "react";
-import { Provider } from "react-redux";
-import "rxjs";
-import { App } from "./containers/App";
-import { store } from "./store";
-
-const theme = createMuiTheme();
+import * as React from 'react';
+import { Provider } from 'react-redux';
+import 'rxjs';
+import { App } from './containers/App';
+import { store } from './store';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
